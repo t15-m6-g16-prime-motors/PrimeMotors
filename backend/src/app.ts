49d8ec4b54febb7ca59a655768c28aedeb26d5aa
Juliano Santos <julianosantosdev@outlook.com>
2023-08-11 14:39:35 -0300

    add: cors settings

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,18 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
+import cors from 'cors';
 
-import { carRoutes, carRoutesFilter } from "./routes/cars.routes";
+import { carRoutes, carRoutesFilter } from './routes/cars.routes';
 
 const app: Application = express();
 app.use(express.json());
 
-app.use("/cars", carRoutes);
-app.use("/carsFilter", carRoutesFilter)
+app.use(
+  cors({
+    origin: process.env.VITE_URL,
+  })
+);
+
+app.use('/cars', carRoutes);
+app.use('/carsFilter', carRoutesFilter);
 
 export default app;
