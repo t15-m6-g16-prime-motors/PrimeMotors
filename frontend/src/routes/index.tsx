434d900141470eb671992afbc0695f7386d2d658
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-08-17 14:57:41 -0300

    feat: add page and style UserProfilePage

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from '../pages/HomePage';
 import { RegisterUserPage } from '../pages/RegisterUserPage';
+import { UserProfilePage } from '../pages/UserProfilePage';
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route path='/' element={<HomePage />} />
+      <Route path='/profile' element={<UserProfilePage />} />
       <Route path='/register' element={<RegisterUserPage />} />
     </Routes>
   );
