fbce9a44d847592d0c88f1ca4491c1c5ddef93c9
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-08-28 17:10:01 -0300

    feat: add get,update,delete,interfaces and controllers by comment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,26 +1,28 @@
-import "reflect-metadata"
-import "express-async-errors"
-import express, { Application } from "express";
-import cors from "cors";
-import { carRoutes, carRoutesFilter } from "./routes/cars.routes";
-import { loginRoutes } from "./routes/login.routes";
-import userRoutes from "./routes/users.routes";
-import handleErrors from "./errors/handleErrors";
+import 'reflect-metadata';
+import 'express-async-errors';
+import express, { Application } from 'express';
+import cors from 'cors';
+import userRoutes from './routes/users.routes';
+import handleErrors from './errors/handleErrors';
+import { carRoutes, carRoutesFilter } from './routes/cars.routes';
+import { loginRoutes } from './routes/login.routes';
+import { commentRoutes } from './routes/comments.routes';
 
 const app: Application = express();
 app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.VITE_URL,
+    origin: process.env.VITE_URL
   })
 );
 
-app.use("/cars", carRoutes);
-app.use("/carsFilter", carRoutesFilter);
-app.use("/users", userRoutes);
-app.use("/login", loginRoutes);
+app.use('/cars', carRoutes);
+app.use('/comments', commentRoutes);
+app.use('/carsFilter', carRoutesFilter);
+app.use('/users', userRoutes);
+app.use('/login', loginRoutes);
 
-app.use(handleErrors)
+app.use(handleErrors);
 
 export default app;
--- backend/src/schemas/comments.schemas.ts
+++ backend/src/schemas/comments.schemas.ts
@@ -1,9 +1,29 @@
-import { z } from "zod"
+import { z } from 'zod';
+import { userSchema } from './users.schemas';
 
 const commentSchema = z.object({
-    id: z.number(),
-    comment: z.string(),
-    created_at: z.union([z.date(), z.string()]),
+  id: z.number(),
+  comment: z.string(),
+  created_at: z.union([z.date(), z.string()])
 });
 
-export { commentSchema }
+const commentSchemaResponse = commentSchema.extend({
+  user: userSchema.omit({ password: true })
+});
+
+const commentsSchemaResponse = z.array(commentSchemaResponse);
+
+const commentSchemaRequest = commentSchema.omit({ id: true, created_at: true });
+
+const commentSchemaUpdateRequest = commentSchemaRequest.optional();
+
+const commentSchemaUpdateResponse = commentSchemaResponse;
+
+export {
+  commentSchema,
+  commentSchemaRequest,
+  commentSchemaResponse,
+  commentsSchemaResponse,
+  commentSchemaUpdateRequest,
+  commentSchemaUpdateResponse
+};
