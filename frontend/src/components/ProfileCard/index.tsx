3ee76b308707beee9e292cdc42e6ea82f4959473
Juliano Santos <julianosantosdev@outlook.com>
2023-08-31 09:43:27 -0300

    feat: integrate edit car with edit button on profile page

--- frontend/src/components/ProfileCard/index.tsx
+++ frontend/src/components/ProfileCard/index.tsx
@@ -4,7 +4,7 @@ import { ICardProps } from '../../interfaces';
 import { StyledProfileCardContainer } from './style';
 
 export const ProfileCard = ({ car }: ICardProps) => {
-  const { setSelectedCar } = useCar();
+  const { setSelectedCar, findCarToEdit } = useCar();
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -13,40 +13,51 @@ export const ProfileCard = ({ car }: ICardProps) => {
   };
 
   return (
-    <StyledProfileCardContainer>
-      <div className='imageContainer'>
-        <img src={car.picture.coverImage} alt='Car image' />
-        {car.published ? (
-          <div className='activeTag text-style-text-body-2-500'>Ativo</div>
-        ) : (
-          <div className='inactiveTag text-style-text-body-2-500'>Inativo</div>
-        )}
-      </div>
-      <h3 className='title heading-7-600'>
-        {car.brand} - {car.model}
-      </h3>
-      <p className='description text-style-text-body-2-400'>
-        {car.description}
-      </p>
-      <div className='carInfo'>
-        <div className='tagsContainer text-style-text-body-2-500'>
-          <p className='tag'>{car.kilometrage.toLocaleString('pt-BR')} km</p>
-          <p className='tag'>{car.year}</p>
+    <>
+      <StyledProfileCardContainer>
+        <div className='imageContainer'>
+          <img src={car.picture.coverImage} alt='Car image' />
+          {car.published ? (
+            <div className='activeTag text-style-text-body-2-500'>Ativo</div>
+          ) : (
+            <div className='inactiveTag text-style-text-body-2-500'>
+              Inativo
+            </div>
+          )}
         </div>
-
-        <p className='price heading-7-500'>
-          {parseFloat(car.price).toLocaleString('pt-BR', {
-            style: 'currency',
-            currency: 'BRL'
-          })}
+        <h3 className='title heading-7-600'>
+          {car.brand} - {car.model}
+        </h3>
+        <p className='description text-style-text-body-2-400'>
+          {car.description}
         </p>
-      </div>
-      <div className='buttonsContainer'>
-        <button className='editButton'>Editar</button>
-        <button className='detailButton' onClick={handleClick}>
-          Ver detalhes
-        </button>
-      </div>
-    </StyledProfileCardContainer>
+        <div className='carInfo'>
+          <div className='tagsContainer text-style-text-body-2-500'>
+            <p className='tag'>{car.kilometrage.toLocaleString('pt-BR')} km</p>
+            <p className='tag'>{car.year}</p>
+          </div>
+
+          <p className='price heading-7-500'>
+            {parseFloat(car.price).toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            })}
+          </p>
+        </div>
+        <div className='buttonsContainer'>
+          <button
+            onClick={() => {
+              findCarToEdit(car.id);
+            }}
+            className='editButton'
+          >
+            Editar
+          </button>
+          <button className='detailButton' onClick={handleClick}>
+            Ver detalhes
+          </button>
+        </div>
+      </StyledProfileCardContainer>
+    </>
   );
 };
