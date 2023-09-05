ffdfdb6eeb79d3c0a74c650bd5776eb2391d1688
Eduardo Henrique <eduardohairescampos@gmail.com>
2023-09-05 16:17:03 -0300

    feat:add render deploy

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,27 +1,36 @@
-import "reflect-metadata";
-import path from "path";
-import { DataSource, DataSourceOptions } from "typeorm";
-import "dotenv/config";
+import 'reflect-metadata';
+import path from 'path';
+import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
 
 const dataSourceConfig = (): DataSourceOptions => {
-  const entitiesPath: string = path.join(__dirname, "./entities/**.{ts,js}");
+  const entitiesPath: string = path.join(__dirname, './entities/**.{ts,js}');
   const migrationsPath: string = path.join(
     __dirname,
-    "./migrations/**.{ts,js}"
+    './migrations/**.{ts,js}'
   );
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
 
   const dbUrl: string | undefined = process.env.DATABASE_URL;
   if (!dbUrl) {
-    throw new Error("Env var DATABASE_URL does not exists.");
+    throw new Error('Env var DATABASE_URL does not exists.');
   }
 
+  if (nodeEnv === 'production') {
+    return {
+      type: 'postgres',
+      url: process.env.DATABASE_URL!,
+      entities: [entitiesPath],
+      migrations: [migrationsPath]
+    };
+  }
   return {
-    type: "postgres",
+    type: 'postgres',
     url: dbUrl,
     synchronize: false,
     logging: true,
     entities: [entitiesPath],
-    migrations: [migrationsPath],
+    migrations: [migrationsPath]
   };
 };
 
